8835462b74f2b2224aa505f698375ca5aa38a762
vince <vincent.harpin@orange.fr>
2023-01-10 15:20:48 +0100

    exercice 136

--- index.php
+++ index.php
@@ -1,18 +1,21 @@
 <?php
 
 // Premiere ligne
-echo 10 5;
+echo 10 * 5;
 
 //Deuxieme ligne
-echo 10 2;
+echo 10 / 2;
+
+$a = 2;
+$b = 2;
 
 //Troisieme ligne
-if($a $b) {
+if($a === $b) {
     echo "<br><br>a est identique à b";
 }
 
 //Quatrieme ligne
-if($a $b) {
+if($a !== $b) {
     echo"<br><br>a n'est pas identique à b";
 }
 
@@ -20,7 +23,7 @@ if($a $b) {
 //Cinquieme ligne
 $arr1 = ["machin", "truc"];
 $arr2 = ["truc", "machin"];
-if($arr1 $arr2) {
+if($arr1 === $arr2) {
     echo "<br><br>les tableaux ont le même contenu";
 }
 
